5a18a8540e8e14e2cc74f73d9620ec27272e5eb5
kambleaishwarya <aishwaryakamble925@gmail.com>
2025-01-30 15:52:50 +0530

    added changes in bus-ride crud #3

--- src/main/java/com/pinnacle/bus/repository/MySQLRepository.java
+++ src/main/java/com/pinnacle/bus/repository/MySQLRepository.java
@@ -1,9 +1,9 @@
 package com.pinnacle.bus.repository;
 
-
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -22,30 +22,29 @@ public class MySQLRepository {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    // Insert a fare
     public int insertFare(Fare fare) {
         String sql = "INSERT INTO fare (id, route_id, from_location_id, to_location_id, price, created_by_user_id) " +
                 "VALUES (?, ?, ?, ?, ?, ?)";
-        return jdbcTemplate.update(sql, fare.getId(), fare.getRouteId(), fare.getFromLocationId(),
+
+        String generatedId = UUID.randomUUID().toString();
+
+        return jdbcTemplate.update(sql, generatedId, fare.getRouteId(), fare.getFromLocationId(),
                 fare.getToLocationId(), fare.getPrice(), fare.getCreatedByUserId());
     }
 
-    // Get fare by ID
     public Fare getFareById(String id) {
         String sql = "SELECT * FROM fare WHERE id = ?";
-        
-        // Use RowMapper correctly without referring to instance variables
-        return jdbcTemplate.queryForObject(sql, new Object[]{id}, new FareRowMapper());
+
+        return jdbcTemplate.queryForObject(sql, new Object[] { id }, new FareRowMapper());
     }
 
     // List all fares
     public List<Fare> getAllFares() {
         String sql = "SELECT * FROM fare";
-        
+
         return jdbcTemplate.query(sql, new FareRowMapper());
     }
 
-    // RowMapper implementation for mapping Fare objects
     private static class FareRowMapper implements RowMapper<Fare> {
         @Override
         public Fare mapRow(ResultSet rs, int rowNum) throws SQLException {
@@ -55,8 +54,7 @@ public class MySQLRepository {
                     rs.getString("from_location_id"),
                     rs.getString("to_location_id"),
                     rs.getFloat("price"),
-                    rs.getString("created_by_user_id")
-            );
+                    rs.getString("created_by_user_id"));
         }
     }
 }
